5bcb3ab1ff6e1238b37e3e283c0ca60d92f6027d
asen <zhanghongsen@linzikg.com>
2023-09-12 17:25:20 +0800

    增加文档配置

--- site/docs/.vitepress/config.js
+++ site/docs/.vitepress/config.js
@@ -1,3 +1,5 @@
+
+
 export default {
   title: "asen-ui",
   description: "程序员阿森的一次自己搭建组件库的初体验",
@@ -5,48 +7,48 @@ export default {
   themeConfig: {
     siteTitle: "vitepress",
     nav: [
-      { text: "指南", link: "/guild/" },
-      { text: "组件", link: "/components/button/" },
+      { text: "指南", link: "/guide/" },
+      { text: "组件", link: "/components/" },
     ],
     socialLinks: [
       { icon: "github", link: "https://github.com/z-h-a-n-g/dome-ui.git" },
     ],
-  },
-  sidebar: {
-    "/guild/": [
-      {
-        text: "基础",
-        items: [
-          {
-            text: "安装",
-            link: "/guild/installation",
-          },
-          {
-            text: "快速开始",
-            link: "/guild/quickstart",
-          },
-        ],
-      },
-      {
-        text: "进阶",
-        items: [
-          {
-            text: "xx",
-            link: "/xx",
-          },
-        ],
-      },
-    ],
-    "/components/": [
-      {
-        text: "基础组件",
-        items: [
-          {
-            text: "Button",
-            link: "/components/button",
-          }
-        ],
-      }
-    ]
-  }
+    sidebar: {
+      "/guide/": [
+        {
+          text: "基础",
+          items: [
+            {
+              text: "安装",
+              link: "/guild/installation",
+            },
+            {
+              text: "快速开始",
+              link: "/guild/quickstart",
+            },
+          ],
+        },
+        {
+          text: "进阶",
+          items: [
+            {
+              text: "xx",
+              link: "/xx",
+            },
+          ],
+        },
+      ],
+      "/components/": [
+        {
+          text: "基础组件",
+          items: [
+            {
+              text: "Button",
+              link: "/components/button",
+            }
+          ],
+        }
+      ]
+    }
+  }, 
 };
--- site/docs/index.md
+++ site/docs/index.md
@@ -1,1 +1,28 @@
-# Hello Easyest
+---
+layout: home
+
+title: 选项卡标题
+titleTemplate: 选项卡描述
+editLink: true
+lastUpdated: true
+hero:
+  name: asen-ui
+  tagline: asen-ui，一套为了更快的版本迭代而开发的业务组件库
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/
+    - theme: alt
+      text: 组件
+      link: /components/
+features:
+  - icon: 🔨
+    title: 功能/特点 1
+    details: 功能/特点 1 具体描述信息。
+  - icon: 🧩
+    title: 功能/特点 2
+    details: 功能/特点 2 具体描述信息。
+  - icon: ✈️
+    title: 功能/特点 3。
+    details: 功能/特点 3 具体描述信息。
+---
